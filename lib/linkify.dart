df5e1d61b0b7245d7933161735c02f9add993b75
Nightbl927 <37490140+Nightbl927@users.noreply.github.com>
2022-03-05 14:52:02 -0500

    Update linkify.dart

--- lib/linkify.dart
+++ lib/linkify.dart
@@ -6,6 +6,8 @@ export 'package:linkify/src/email.dart' show EmailLinkifier, EmailElement;
 export 'package:linkify/src/url.dart' show UrlLinkifier, UrlElement;
 export 'package:linkify/src/user_tag.dart'
     show UserTagLinkifier, UserTagElement;
+export 'package:linkify/src/phone_number.dart'
+    show PhoneNumberLinkifier, PhoneNumberElement;
 
 abstract class LinkifyElement {
   final String text;
